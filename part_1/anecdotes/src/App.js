caaaed84c5a739054fafa9fc3669d84e6c97af70
Sashka <ivanov5oleksandr@gmail.com>
2020-06-18 00:00:07 +0300

    1.23: anecdotes step1

---
+++ part_1/anecdotes/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const anecdotes = [
+  'If it hurts, do it more ofter',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+];
+
+function App() {
+  const [selected, setSelected] = useState(0);
+  return (
+    <div>
+      <div>{anecdotes[selected]}</div>
+      <button onClick={() => nextAnecdote(anecdotes, setSelected)}>next anecdote</button>
+    </div>
+  );
+}
+
+function nextAnecdote(anecdotes, handler) {
+  const { floor, random } = Math;
+  handler(floor(random() * anecdotes.length));
+}
+
+export default App;
