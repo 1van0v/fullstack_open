8c9ec5958de9f6c0ce5a175ef9953ad9d225ef47
Sashka <ivanov5oleksandr@gmail.com>
2020-06-17 20:31:22 +0300

    1.7: unicafe step2

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -4,6 +4,9 @@ export default function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const all = good + neutral + bad;
+
+  const average = (good - bad) / all;
 
   return (
     <>
@@ -15,6 +18,9 @@ export default function App() {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {average || 0}</div>
+      <div>positive {(good / all) * 100 || 0}%</div>
     </>
   );
 }
