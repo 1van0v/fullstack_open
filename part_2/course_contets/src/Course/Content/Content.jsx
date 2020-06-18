9b733b29f2e8284d7ab3f66741c4525df2658d1a
Sashka <ivanov5oleksandr@gmail.com>
2020-06-18 20:34:24 +0300

    2.2: course contents step7

--- part_2/course_contets/src/Course/Content/Content.jsx
+++ part_2/course_contets/src/Course/Content/Content.jsx
@@ -3,14 +3,22 @@ import React from 'react';
 import Part from './Part';
 
 export default function Content({ parts }) {
-  console.log('content', parts);
+  let total = 0;
+
   return (
     <table>
       <tbody>
-        {parts.map((part) => (
-          <Part key={part.id} part={part} />
-        ))}
+        {parts.map((part) => {
+          total += part.exercises;
+
+          return <Part key={part.id} part={part} />;
+        })}
       </tbody>
+      <tfoot>
+        <tr colSpan={2}>
+          <th>total of {total} exercises</th>
+        </tr>
+      </tfoot>
     </table>
   );
 }
