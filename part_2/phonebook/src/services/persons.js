39601fa2da14b44c0f3de352a426f8627b624391
Sashka <ivanov5oleksandr@gmail.com>
2020-06-29 22:36:13 +0300

    changes for part_3

--- part_2/phonebook/src/services/persons.js
+++ part_2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://127.0.0.1:3001/persons';
+const baseUrl = process.env.REACT_APP_API_URL || 'http://127.0.0.1:3001/persons';
 
 function getPersons() {
   return axios.get(baseUrl).then((response) => response.data);
