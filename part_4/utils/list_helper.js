eddb9127b97fa285fe0e97a67b72d77751cc3267
Sashka <ivanov5oleksandr@gmail.com>
2020-10-20 22:47:39 +0300

    4.4: helper functions and unit tests, step2

--- part_4/utils/list_helper.js
+++ part_4/utils/list_helper.js
@@ -2,6 +2,11 @@ function dummy(blogs) {
   return 1;
 }
 
+function totalLikes(blogs) {
+  return blogs.reduce((acc, item) => acc + item.likes, 0);
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 };
