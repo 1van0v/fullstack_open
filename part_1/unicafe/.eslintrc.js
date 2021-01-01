d79a3a71569070a3adb20f83bc0c2e24d271e351
Sashka <ivanov5oleksandr@gmail.com>
2021-01-01 19:11:44 +0200

    6.1: unicafe revisited, step1

--- part_1/unicafe/.eslintrc.js
+++ part_1/unicafe/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es6: true
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier', 'plugin:jest/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
@@ -15,7 +15,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module'
   },
-  plugins: ['react'],
+  plugins: ['react', 'jest'],
   rules: {
     'react/prop-types': 0
   },
