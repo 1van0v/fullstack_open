917c4fbcb2c37e490f4c039d673c0c27ff654cb6
Sashka <ivanov5oleksandr@gmail.com>
2021-01-03 19:12:20 +0200

    6.19 anecdotes and connect, step1

--- part_6/redux-anecdotes/src/components/AnecdoteList.js
+++ part_6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,31 +1,37 @@
 import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { connect } from "react-redux";
 
 import anecdoteActions from "../store/actions/anecdoteActions";
 
-export default function AnecdoteForm() {
-  const anecdotes = useSelector(({ anecdotes, filter }) =>
-    filter
-      ? anecdotes.filter((i) => i.content.toLowerCase().includes(filter))
-      : anecdotes
-  );
-  const dispatch = useDispatch();
-
+function AnecdoteList(props) {
   useEffect(() => {
-    dispatch(anecdoteActions.fetch());
-  }, [dispatch]);
+    props.fetch();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  const vote = (anecdote) => {
-    dispatch(anecdoteActions.vote(anecdote));
-  };
-
-  return anecdotes.map((anecdote) => (
+  return props.anecdotes.map((anecdote) => (
     <div key={anecdote.id}>
       <div>{anecdote.content}</div>
       <div>
         has {anecdote.votes}
-        <button onClick={() => vote(anecdote)}>vote</button>
+        <button onClick={() => props.vote(anecdote)}>vote</button>
       </div>
     </div>
   ));
 }
+
+const mapStateToProps = ({ anecdotes, filter }) => {
+  return {
+    anecdotes: filter
+      ? anecdotes.filter((i) => i.content.toLowerCase().includes(filter))
+      : anecdotes,
+    filter,
+  };
+};
+
+const ConnectedAnecdoteList = connect(mapStateToProps, {
+  fetch: anecdoteActions.fetch,
+  vote: anecdoteActions.vote,
+})(AnecdoteList);
+
+export default ConnectedAnecdoteList;
